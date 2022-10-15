5c0048a1d65b9a888e9d57a7a4a95c5ed2e64e4e
Jose <joseneto.jn1606@gmail.com>
2022-10-15 13:14:21 -0300

    Exemplos Java

---
+++ Potência Tech powered by iFood - Java Beginners/Exemplos/Tipos Variáveis/exemplos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,6 @@
+public class TiposVariaveis{
+    public static void main(String[] args) throws Exception {
+        String nome = "José";
+        System.out.println("Meu nome é: " + nome);
+    }
+}
